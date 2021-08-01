b7eca04d59b1c9e4eb98819acbc1e218f09780d9
Ceithir <ceathramh@gmail.com>
2021-08-01 13:49:51 +0200

    cm <=> feet/inches

--- converter.js
+++ converter.js
@@ -1,4 +1,21 @@
 function convert(text) {
+  var matches;
+
+  matches = text.match(/([0-9]+)\s*cm/);
+  if (matches !== null) {
+    var cmHeight = parseInt(matches[1]);
+    var feet = Math.floor(cmHeight / 30.48);
+    var inches = Math.round((cmHeight - feet * 30.48) / 2.54);
+    return feet + "' " + inches + '"';
+  }
+
+  matches = text.match(/([0-9]+)'\s*([0-9]+)"/);
+  if (matches !== null) {
+    var feet = parseInt(matches[1]);
+    var inches = parseInt(matches[2]);
+    return Math.round(feet * 30.48 + inches * 2.54) + "cm";
+  }
+
   return text;
 }
 module.exports = convert;
--- converter.test.js
+++ converter.test.js
@@ -3,3 +3,14 @@ const convert = require("./converter");
 test("non-sensical entries are returned as is", () => {
   expect(convert("1000efhkfuef")).toBe("1000efhkfuef");
 });
+
+describe("height", () => {
+  it("converts cm into imperial", () => {
+    expect(convert("160cm")).toBe(`5' 3"`);
+    expect(convert("210 cm")).toBe(`6' 11"`);
+  });
+  it("converts imperial into cm", () => {
+    expect(convert(`5' 3"`)).toBe("160cm");
+    expect(convert(`6'11"`)).toBe("211cm");
+  });
+});
